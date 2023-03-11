009f0d3975f2ac35862b9893e518bac3f45fde87
saint-hong <innercabinet@gmail.com>
2023-03-11 10:47:48 +0900

    update functions

--- functions/modeling_non_const.py
+++ functions/modeling_non_const.py
@@ -1,12 +1,12 @@
 """
 <상수항 없는 데이터 프레임을 사용한 모델링 함수>
 
-from_formula 매서드를 사용하여 OLS 모델을 생성하기 위한 함수
+OLS 패키지의 from_formula 매서드를 사용하여 OLS 모델을 생성하기 위한 함수
 
 X와 y 데이터가 포함된 데이터 프레임을 사용 :
     단, 데이터에 상수항이 없어야 함
 
-각 모델링 실험에서 주로  사용할 함수    
+ANOVA(분산분석)에서 사용할 수 있음
 """
 
 def modeling_non_const(formula, data) :
