790f60bde35117b0b49ecc982cd4d03d2f38527f
saint-hong <innercabinet@gmail.com>
2023-03-11 10:46:01 +0900

    update functions

---
+++ functions/modeling_non_const.py
@@ -0,0 +1,30 @@
+"""
+<상수항 없는 데이터 프레임을 사용한 모델링 함수>
+
+from_formula 매서드를 사용하여 OLS 모델을 생성하기 위한 함수
+
+X와 y 데이터가 포함된 데이터 프레임을 사용 :
+    단, 데이터에 상수항이 없어야 함
+
+각 모델링 실험에서 주로  사용할 함수    
+"""
+
+def modeling_non_const(formula, data) :
+
+    """
+    * formula : "MEDV ~ scale(CRIM) + scale(ZN) + ..." : str
+    * data : 상수항이 없는 X 데이터와 y 데이터로 결합된 데이터 프레임
+    * return : 모델의 모수추정 결과 반환
+    """
+
+    model = sm.OLS.from_formula(formula, data=data)
+    result = model.fit()
+
+    return model, result
+
+
+
+
+
+
+
